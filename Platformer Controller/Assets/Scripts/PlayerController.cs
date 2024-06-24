69e4ec88a26141b802431784ab2bc0ff588c8668
Jostlk <evgenijlopyrev29@gmail.com>
2024-06-24 19:14:19 +0300

    Fix Long Jump

--- Platformer Controller/Assets/Scripts/PlayerController.cs
+++ Platformer Controller/Assets/Scripts/PlayerController.cs
@@ -10,8 +10,6 @@ public class PlayerController : MonoBehaviour
     public float RunSpeed = 8;
     private float _currentSpeed;
     public float JumpForce = 3;
-    public float JumpTime = 2;
-    private float _currentJumpTime = 0;
     private bool _maxJump = false;
     private float _jump = 0;
     private float _gravity = -9.81f;
@@ -24,17 +22,23 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) && !_maxJump)
         {
-            _currentJumpTime += Time.deltaTime;
-            if (_currentJumpTime >= JumpTime)
+            if (_jump <= 0)
             {
                 _maxJump = true;
-                _currentJumpTime = 0;
             }
+            _jump -= Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
             _jump = JumpForce;
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             _maxJump = true;
+            while (_jump > 0)
+            {
+                _jump -= Time.deltaTime;
+            }
         }
         if (Input.GetKeyDown(KeyCode.LeftShift) && _isGrounded)
         {
